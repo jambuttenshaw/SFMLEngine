82e31212403874cecebd45d125b48c643558a7e6
Jamie Buttenshaw <sigton.scratch@gmail.com>
2021-03-02 17:36:54 +0000

    still restructuring and improving build system

---
+++ SFMLEngine/premake5.lua
@@ -0,0 +1,77 @@
+project "SFMLEngine"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp",
+		"vendor/tracy/TracyClient.cpp"
+	}
+
+	defines
+	{
+		"SFML_STATIC",
+		"GLEW_STATIC",
+		"TRACY_ENABLE"
+	}
+
+	includedirs
+	{
+		"%{wks.location}/SFMLEngine/src",
+		"%{IncludeDir.SFML}",
+		"%{IncludeDir.spdlog}",
+		"%{IncludeDir.glew}",
+		"%{IncludeDir.tracy}",
+		"%{IncludeDir.json}"
+	}
+	
+	libdirs
+	{
+		"%{LibraryDir.SFML}",
+		"%{LibraryDir.glew}"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+		defines 
+		{
+			"SFMLENGINE_ENABLE_ASSERTS",
+			"SFMLENGINE_DEBUG",
+			"_CRTDBG_MAP_ALLOC"
+		}
+		
+		links
+		{
+			"winmm.lib",
+			"opengl32.lib",
+			"freetype.lib",
+			"glew32s.lib",
+			"sfml-graphics-s-d.lib",
+			"sfml-window-s-d.lib",
+			"sfml-system-s-d.lib"
+
+		}
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+		
+		links
+		{
+			"winmm.lib",
+			"opengl32.lib",
+			"freetype.lib",
+			"glew32s.lib",
+			"sfml-graphics-s.lib",
+			"sfml-window-s.lib",
+			"sfml-system-s.lib"
+		}
---
+++ Sandbox/premake5.lua
@@ -0,0 +1,81 @@
+project "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+
+	
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+	
+	files
+	{
+		"src/**.h",
+		"src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{wks.location}/SFMLEngine/src",
+		"assets/scripts",
+		"%{IncludeDir.SFML}",
+		"%{IncludeDir.spdlog}",
+		"%{IncludeDir.glew}",
+		"%{IncludeDir.tracy}",
+		"%{IncludeDir.json}"
+	}
+
+	links
+	{
+		"SFMLEngine"
+	}
+
+	libdirs
+	{
+		"%{LibraryDir.SFML}",
+		"%{LibraryDir.glew}"
+	}
+	
+	defines
+	{
+		"SFML_STATIC"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+		defines 
+		{
+			"SFMLENGINE_ENABLE_ASSERTS",
+			"SFMLENGINE_DEBUG",
+			"_CRTDBG_MAP_ALLOC"
+		}
+		
+		links
+		{
+			"winmm.lib",
+			"opengl32.lib",
+			"freetype.lib",
+			"glew32s.lib",
+			"sfml-graphics-s-d.lib",
+			"sfml-window-s-d.lib",
+			"sfml-system-s-d.lib"
+
+		}
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+		
+		links
+		{
+			"winmm.lib",
+			"opengl32.lib",
+			"freetype.lib",
+			"glew32s.lib",
+			"sfml-graphics-s.lib",
+			"sfml-window-s.lib",
+			"sfml-system-s.lib"
+		}
