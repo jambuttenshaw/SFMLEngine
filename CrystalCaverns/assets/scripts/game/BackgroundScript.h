075c821407c770fcb292848b67cefa5cd6b827aa
Jamie Buttenshaw <sigton.scratch@gmail.com>
2021-05-02 12:39:13 +0100

    made parallax background wrap around infinitely

--- CrystalCaverns/assets/scripts/game/BackgroundScript.h
+++ CrystalCaverns/assets/scripts/game/BackgroundScript.h
@@ -18,6 +18,20 @@ public:
 	{
 		sf::Vector2f pos{ m_MoveFactor * m_CameraTransform->GetPosition().x, m_CameraTransform->GetPosition().y };
 		m_Transform->SetPosition(pos + m_Offset);
+
+		// work out if this tile has gone off screen
+		// if it has then wrap it round to the other side
+		// we can work this out by transforming its position into the cameras local space
+		pos = m_CameraTransform->GetWorldToLocalTransformMatrix().transformPoint(m_Transform->GetPosition());
+		// if the right of the object is off the left of the screen
+		if (pos.x + 256 <= -950)
+		{
+			m_Transform->Translate({ 256.0f * s_NumBackgroundTiles, 0 });
+		}
+		else if (pos.x >= 950)
+		{
+			m_Transform->Translate({ -256.0f * s_NumBackgroundTiles, 0 });
+		}
 	}
 
 	void SetMoveFactor(float factor)
@@ -27,6 +41,8 @@ public:
 		else
 			m_MoveFactor = 1.0f / factor;
 	}
+	
+	static inline const int s_NumBackgroundTiles = 7;
 
 private:
 	Transform* m_CameraTransform = nullptr;
@@ -46,14 +62,13 @@ public:
 		ResourceID background2 = Texture::Create("assets/textures/background2.png");
 		ResourceID backgroundNormals = Texture::Create("assets/textures/background_n.png");
 
-		const int numBackgroundTiles = 7;
-		for (int x = 0; x < numBackgroundTiles; x++)
+		for (int x = 0; x < BackgroundMover::s_NumBackgroundTiles; x++)
 		{
 			Entity newBackground1 = CreateEntity();
 			Entity newBackground2 = CreateEntity();
 
 			Transform t;
-			t.SetPosition({ (256.0f * (x - (numBackgroundTiles / 2))) - 128.0f, -540.0f });
+			t.SetPosition({ (256.0f * (x - (BackgroundMover::s_NumBackgroundTiles / 2))) - 128.0f, -540.0f });
 			AddComponent(newBackground1, t);
 			AddComponent(newBackground2, t);
 
@@ -77,4 +92,5 @@ public:
 		}
 		
 	}
+
 };
