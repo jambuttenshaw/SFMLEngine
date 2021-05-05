2ca8b7ca1495902baa49e6a014dbf12c824c2e69
Sigton <sigton.scratch@gmail.com>
2021-05-05 21:58:21 +0100

    adding commentary

--- CrystalCaverns/src/ExampleProjectApp.cpp
+++ CrystalCaverns/src/ExampleProjectApp.cpp
@@ -15,24 +15,34 @@
 
 using namespace SFMLEngine;
 
-class SandboxApp : public Application
+class CrystalCavernsApp : public Application
 {
 public:
-	SandboxApp()
+	CrystalCavernsApp()
+		// give the application a title and a default window size
 		: Application("Crystal Caverns", sf::Vector2i(1200, 675))
 	{
+		// set the clear colour
+		// black blends in best if there are any gaps
 		SetClearColor(sf::Color::Black);
+		// use vsync to limit the framerate
 		SetVSync(true);
 		
 		// set up the data that should exist between scenes
+		// this allocates a bool and an instance of player data
+		// and associates a string with these allocations so they can be retrieved
+		// and utilized later
 		DataStore::CreatePersistentData("loadedLevel1", false);
 		DataStore::CreatePersistentData("playerData", PlayerData{ });
 		
 
+		// initialise the level factory to create level components
 		LevelFactory::Init();
 
 
 		// load the font
+		// it is used by multiple scenes so we just load it in advance of any of them
+		// to have it available to all of them
 		FontLibrary::LoadNewFont("arcade", "assets/fonts/ARCADECLASSIC.TTF");
 
 
@@ -47,10 +57,13 @@ public:
 		AudioSystem::SetMusicLooping(true);
 		AudioSystem::SetMusicVolume(30);
 
+
+		// then finally load the splash screen
+		// the splash screen handles transition to the menu, which handles transition into the game etc...
 		LoadScene<SplashScreen>(LoadSceneMode::Single);
 	}
 
-	~SandboxApp()
+	~CrystalCavernsApp()
 	{
 		// contents of the data store should be destroyed manually
 		// so they can be cast to the correct type before deletion
@@ -59,7 +72,10 @@ public:
 	}
 };
 
+
+// CreateApplication is the handle into which the application can register itself with the engine
+// using this function the engine knows exactly what type of application it is to create and run
 Application* SFMLEngine::CreateApplication()
 {
-	return new SandboxApp;
+	return new CrystalCavernsApp;
 }
